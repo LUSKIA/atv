ca713db465976616e4d3d2b821708969908335f1
Lucas Guilherme Pereira Silva <108810494+LUSKIA@users.noreply.github.com>
2023-06-14 12:38:14 -0300

    Add files via upload

---
+++ alal/Menu/main.js
@@ -0,0 +1,23 @@
+// const seguidores = document.querySelector("#seguidores");
+
+// window.addEventListener("DOMContentLoaded", async () => {
+//   await fetch("https://api.github.com/users/LUSKIA")
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     });
+
+//   await fetch("https://api.github.com/users/LUSKIA/followers")
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//         data.map((item) => {
+//             seguidores.innerHTML += `<img src="${item.avatar_url    }"></img>
+//             <p>${item.login}</p>`;
+
+//         });
+//     });
+// });
